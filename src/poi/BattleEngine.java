ba7fcc6f6b1fa05a13a9c8763aa6c10512115e81
srbr <srbr@SRBR-LAP.oradev.oraclecorp.com>
2018-07-07 17:01:43 +0530

    2 Radars and 1 Aircraft. Average POI

--- src/poi/BattleEngine.java
+++ src/poi/BattleEngine.java
@@ -28,13 +28,14 @@ public class BattleEngine {
         double R;
         double Preceived;
         double aVel;
-        double aircraft_hits = 0;
-        double radar_hits = 0;
+        double aircraft_hits1 = 0;
+        double radar_hits1 = 0;
+        double aircraft_hits2 = 0;
+        double radar_hits2 = 0;
+        double POI1;
+        double POI2;
+        double finalPOI;
         int iteration = 0;
-        int bandADuration;
-        int bandBDuration;
-        int bandCDuration;
-        int bandDDuration;
         CartesianLocation cl;
         
         int j;
@@ -48,16 +49,10 @@ public class BattleEngine {
         // EDIT TO GET DIFFERENT POI
         //
         
-        // Set band durations here.
-        bandADuration = 10;
-        bandBDuration =  0;
-        bandCDuration =  0;
-        bandDDuration =  0;
-        
         // Instantiating the Aircraft and Radar objects here.
-        AirCraft A1 = new AirCraft(1, new CartesianLocation(100, 100, 100), new CartesianLocation(0, 0, 0),
-                aVel);
+        AirCraft A1 = new AirCraft(1, new CartesianLocation(350, 350, 350), new CartesianLocation(0, 0, 0), aVel);
         Radar R1 = new Radar(1, new CartesianLocation(0, 0, 0), 25  * Math.pow(10.0, -6.0), Bands.BANDA);
+        Radar R2 = new Radar(2, new CartesianLocation(1, 2, 3), 25  * Math.pow(10.0, -6.0), Bands.BANDB);
         
         //
         // Total number of ticks the simulation
@@ -91,24 +86,47 @@ public class BattleEngine {
                 
                 if (R1.checkEchoMatrix() == 1) {
 
-                    radar_hits = radar_hits + 1;
+                    radar_hits1 = radar_hits1 + 1;
                     if (R1.getSignalBand() == A1.getRWRBand()) {
-                        aircraft_hits = aircraft_hits + 1;
+                        aircraft_hits1 = aircraft_hits1 + 1;
                     }
                 }  
-                System.out.println(i + ", " + A1.getRWRBand() + ", " + aircraft_hits + ", " + radar_hits + ", " + R1.getRadarAntennaAzimuth());
+                // System.out.println(i + ", " + A1.getRWRBand() + ", " + aircraft_hits1 + ", " + radar_hits1 + ", " + R1.getRadarAntennaAzimuth());
+                
+                
+                R = R2.getCurLoc().distanceCart(cl);
+                R2.setEchoMatrix(A1, R2, R);
+                R2.setEchoMatrixFilledTime((2 * R) / C);
+                
+                if (R2.checkEchoMatrix() == 1) {
+
+                    radar_hits2 = radar_hits2 + 1;
+                    if (R2.getSignalBand() == A1.getRWRBand()) {
+                        aircraft_hits2 = aircraft_hits2 + 1;
+                    }
+                }  
+                //System.out.println(i + ", " + A1.getRWRBand() + ", " + aircraft_hits2 + ", " + radar_hits2 + ", " + R2.getRadarAntennaAzimuth());
+
 
                 // Update the Radar
                 R1.update(i);
-
+                
+                // Update the Radar
+                R2.update(i);
+                
                 // Update the Aircraft.
-
                 A1.update(i);
 
                 
             }
 
-            System.out.println(aircraft_hits + ", " + radar_hits + ", " + (aircraft_hits / radar_hits));
+            POI1 = (aircraft_hits1 / radar_hits1);
+            POI2 = (aircraft_hits2 / radar_hits2);
+            finalPOI = (POI1 + POI2) / 2;
+            
+            System.out.println(aircraft_hits1 + ", " + radar_hits1 + ", " + (aircraft_hits1 / radar_hits1));
+            System.out.println(aircraft_hits2 + ", " + radar_hits2 + ", " + (aircraft_hits2 / radar_hits2));
+            System.out.println(finalPOI);
 
         //}
             
