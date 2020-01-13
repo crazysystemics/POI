09ce77ce023fce43788b91814a40acbeed4d602f
rvjoshi18 <rvjoshi18@hotmail.com>
2020-01-13 16:54:05 +0530

    first few tests

--- AttendGen/AttendGen/AttendGen.cs
+++ AttendGen/AttendGen/AttendGen.cs
@@ -15,10 +15,16 @@ namespace AttendGen
         public int totalCols;
         public string[] colNames;
         public int maxAttn;
+        public int minAttndnce;
         public int ATTEND_MIN, ATTEND_MAX;
 
+        public AttendGen()
+        {
+
+        }
+
         public AttendGen(string infilename, string outfilename,
-                         int tc, int atmin, int atmax)
+                         int tc, int atmin, int atmax, int minatn, int maxatn)
         {
             sr = new StreamReader(infilename);
             sw = new StreamWriter(outfilename);
@@ -26,6 +32,24 @@ namespace AttendGen
             totalCols = tc;
             ATTEND_MIN = atmin;
             ATTEND_MAX = atmax;
+
+            minAttndnce = minatn;
+            maxAttn = maxatn;
+        }
+
+        public void generate()
+        {
+            initHeader(sr.ReadLine());
+
+            string s = "";
+            while((s = sr.ReadLine()) != null)
+            {
+                string outs = this.Calibrate(s);
+                sw.WriteLine(outs);                
+            }
+
+            sr.Close();
+            sw.Close();
         }
 
         public void initHeader(string sheader)
@@ -43,19 +67,43 @@ namespace AttendGen
             sw.WriteLine(s);
         }
 
+        
         public string Calibrate(string ins)
         {
             string outs = String.Empty;                     
             string[] sarr = ins.Split(',');
-            
 
-            int totalAttendance = Convert.ToInt32(sarr[61]); //TODO:PARK IT BASED ON COLUMN NAME
+            //TODO: make a generic function getpos(string colname)
+            int i, postotal;
+            for (i = 0; i < sarr.Length; i++)
+            {
+                if (colNames[i] == "Total")
+                {
+                    break;
+                }
+            }
+            postotal = i;
+
+            int poscondon;
+            for (i = 0; i < sarr.Length; i++)
+            {
+                if (colNames[i] == "Condonation")
+                {
+                    break;
+                }
+            }
+            poscondon = i;
+
+
+
+
+            int totalAttendance = Convert.ToInt32(sarr[i]); //TODO:PARK IT BASED ON COLUMN NAME
             if ((double)totalAttendance / (double)maxAttn > .86)
                 return ins;
 
             int Absnts85 = Convert.ToInt32((double) maxAttn * .14);
             int actAbsnts = maxAttn - totalAttendance;
-            double  absentProb = (Absnts85 / actAbsnts) -  0.01; 
+            double  absentProb = ((double)Absnts85 /(double) actAbsnts) -  0.01; 
                                      //boundary - 1% bias towards not marking student present
                                      // Want to push beyond 85% in one go :-)                              
                                      
@@ -65,25 +113,108 @@ namespace AttendGen
 
             int index = 0;
             int attndnce = 0;
+            Random rn = new Random();
+            int endval = (int)((double)maxAttn * 0.86) + (rn.Next()) % 5;
+
             Random r = new Random();
             foreach (string s in sarr)
             {
-
-                if (index >=  ATTEND_MIN && index < ATTEND_MAX)
+                if (index >= ATTEND_MIN && index <= ATTEND_MAX)
                 {
-                    if (s == "A" && r.Next() < absentProb)
+                    if ((postotal - 1) - index + 1 > (endval - attndnce + 1))
                     {
-                        outs += s + ",";
+                        //you can still leave some A's toss and flip            
+                        if (s == "A" && r.Next() < absentProb)
+                        {
+                            outs += s + ",";
+                        }
+                        else
+                        {
+                            attndnce++;
+                            outs += attndnce.ToString() + ",";
+                        }
+                    }
+                    else if  ((postotal - 1) - index + 1 > (endval - attndnce + 1))
+                    {
+                        //we are at critical point. no more randomization
+                        //flip every A                                   
+                        if (s == "A")  
+                        {
+                            attndnce++;
+                            outs += attndnce.ToString() + ",";
+                        }
                     }
                     else
                     {
-                        attndnce++;
-                        outs += attndnce.ToString() + ",";
+                        throw new Exception("not enough spage " + index.ToString());
                     }
-                        
                 }
-            }                  
-            return null;
+                index++;
+            }
+
+            outs += sarr[poscondon] + ",";
+            int total = attndnce + Convert.ToInt32(sarr[poscondon]);
+            double percent = (double)total / (double)maxAttn;
+            if (percent < 0.85)
+                throw new Exception("less than 85");
+            outs += total.ToString() + "," + percent.ToString();
+
+
+            return outs;
+
+        }
+
+        public string CalibrateReverse(string ins)
+        {
+            string outs = String.Empty;
+            string[] sarr = ins.Split(',');
+
+            //TODO: make a generic function getpos(string colname)
+            int i, postotal;
+            for (i = 0; i < sarr.Length; i++)
+            {
+                if (colNames[i] == "Total")
+                {
+                    break;
+                }
+            }
+            postotal = i;
+
+            int poscondon;
+            for (i = 0; i < sarr.Length; i++)
+            {
+                if (colNames[i] == "Condonation")
+                {
+                    break;
+                }
+            }
+            poscondon = i;
+
+            int totalAttendance = Convert.ToInt32(sarr[i]); //TODO:PARK IT BASED ON COLUMN NAME
+            if ((double)totalAttendance / (double)maxAttn > .86)
+                return ins;
+
+            Random rn = new Random();
+            int Absnts85 = Convert.ToInt32((double)maxAttn * .14);
+            int actAbsnts = maxAttn - totalAttendance;
+            int reqflips = actAbsnts - Absnts85;
+            double absentProb = ((double)Absnts85 / (double)actAbsnts) - 0.01;
+            int startval = (int)((double)maxAttn * 0.86) + (rn.Next()) % 5;
+            //boundary - 1% bias towards not marking student present
+            // Want to push beyond 85% in one go :-)  
+            Random r = new Random();
+
+            for (i = ATTEND_MAX; i > ATTEND_MIN; i--)
+            {
+                if (reqflips <= 0)
+                    break;                          
+                if (sarr[i] == "A" && r.Next()> 0.86)
+                {
+                    
+                }
+            }
+        
+
         }
     }   
 }
--- AttendGen/AttendGen/Program.cs
+++ AttendGen/AttendGen/Program.cs
@@ -8,8 +8,17 @@ namespace AttendGen
 {
     class Program
     {
+        static public string infn  = 
+            @"C:\One_Drive_1.2\OneDrive\05_Profession\03_Job_Consultancy_Enterprise\02_AIT\01b_70_ACA_common\7ainput.csv";
+
+        static string outfn = "outattend.csv";
+        
+
+        public static AttendGen attgen = new AttendGen(infn, outfn, 67, 6, 64, 4, 57);        
+
         static void Main(string[] args)
         {
+            attgen.generate();        
         }
     }
 }
