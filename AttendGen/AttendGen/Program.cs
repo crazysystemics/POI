ed340cdf1951a47f64ccedd2c202f64909d4e7c1
rvjoshi18 <rvjoshi18@hotmail.com>
2020-02-25 12:37:56 +0530

    debug code - less than 85% and above 100%

--- AttendGen/AttendGen/Program.cs
+++ AttendGen/AttendGen/Program.cs
@@ -8,8 +8,8 @@ namespace AttendGen
 {
     class Program
     {
-        static public string infn_work_pc  = 
-                    @"C:\One_Drive_1.2\OneDrive\05_Profession\03_Job_Consultancy_Enterprise\02_AIT\01b_70_ACA_common\7ainput.csv";
+        static public string infn_work_pc  =
+                    @"C:\One_Drive_1.2\OneDrive\05_Profession\03_Job_Consultancy_Enterprise\02_AIT\01c_ACA_15CS72\01b_70_ACA_common\7ainput.csv";
 
         static public string infn_home_pc =
             @"C:\Users\rvjos\OneDrive_August_2019\OneDrive\05_Profession\03_Job_Consultancy_Enterprise\02_AIT\01b_70_ACA_common\7ainput.csv";
@@ -17,7 +17,7 @@ namespace AttendGen
         static string outfn = "outattend.csv";
         
 
-        public static AttendGen attgen = new AttendGen(infn_home_pc, outfn, 67, 6, 64, 4, 57,6);        
+        public static AttendGen attgen = new AttendGen(infn_work_pc, outfn, 67, 6, 63, 4, 57,6);        
 
         static void Main(string[] args)
         {
