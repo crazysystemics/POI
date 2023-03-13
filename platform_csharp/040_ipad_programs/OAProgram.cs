9f6eb2b8578f1e48eafe08135cd4bbb311597c1a
rvjoshi18 <rvjoshi18@users.noreply.github.com>
2023-03-13 10:14:43 +0530

    Changed name to OAProgram.cs

---
+++ platform_csharp/040_ipad_programs/OAProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ComprehensionQA
+{
+    public class Program
+    {
+        string text = "Joshi";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello " + text);
+        }
+    }
+}
