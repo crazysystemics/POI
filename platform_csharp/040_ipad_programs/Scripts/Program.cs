51a2eb2d2a619a6e74f9fbad9a2d99b0bd0d484a
rvjoshi18 <rvjoshi18@hotmail.com>
2023-03-17 07:50:59 +0530

    Derived isSymmetric

--- platform_csharp/040_ipad_programs/Scripts/Program.cs
+++ platform_csharp/040_ipad_programs/Scripts/Program.cs
@@ -1,16 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Script World!");
 
-Predicate p;
+isSymmetric p;
 
-p = new Predicate();
+
 p=true;
 bool q = p;
-p.Value = true;
-
-
 
 abstract class Predicate
 {
     public bool Value { get; set; }     
+}
+
+class isSymmetric : Predicate
+{
+
 }
