ccb21a67c4986b113b334edbb68e3c9ba18fcf4b
rohitchaoji <53764185+rohitchaoji@users.noreply.github.com>
2023-08-16 10:39:43 +0530

    Updated calculations for movement in opposite direction.

--- BattleSystemNew/BattleSystemNew/Program.cs
+++ BattleSystemNew/BattleSystemNew/Program.cs
@@ -7,7 +7,7 @@ namespace BattleSystemTest_Aug9
         static void Main(string[] args)
         {
             SimulationEngine simulationEngine = new SimulationEngine();
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 100; i++)
             {
                 simulationEngine.RunSimulationEngine();
             }
@@ -19,11 +19,11 @@ namespace BattleSystemTest_Aug9
     {
         public abstract string Type { get; set; }
         public abstract float[] CurrentPosition { get; set; }
-        public abstract List<float[]> Velocities { get; set; }
-        public abstract float[] LegVelocities { get; set; }
-        public abstract List<float[]> FlightPath { get; set; }
         public abstract float[] NewPositionTemp { get; set; }
-        public abstract bool FlightHasStopped { get; set; }
+        public abstract float[] LegVelocities { get; set; }
+        public abstract List<float[]> VehiclePath { get; set; }
+        public abstract List<float[]> Velocities { get; set; }
+        public abstract bool VehicleHasStopped { get; set; }
         public abstract int InLeg { get; set; }
         public abstract float[] Get();
         public abstract void Set();
@@ -34,11 +34,11 @@ namespace BattleSystemTest_Aug9
     {
         public override string Type { get; set; }
         public override float[] CurrentPosition { get; set; }
-        public override List<float[]> Velocities { get; set; }
-        public override float[] LegVelocities { get; set; }
         public override float[] NewPositionTemp { get; set; }
-        public override List<float[]> FlightPath { get; set; }
-        public override bool FlightHasStopped { get; set; }
+        public override float[] LegVelocities { get; set; }
+        public override List<float[]> Velocities { get; set; }
+        public override List<float[]> VehiclePath { get; set; }
+        public override bool VehicleHasStopped { get; set; }
         public override int InLeg { get; set; }
         public override float[] Get()
         {
@@ -64,7 +64,7 @@ namespace BattleSystemTest_Aug9
              * and and array of length (waypoints.Count - 1) representing velocities in each leg as second argument */
 
 
-            this.FlightPath = waypoints;
+            this.VehiclePath = waypoints;
             this.LegVelocities = leg_velocities;
             this.Velocities = new List<float[]>();
             for (int i = 0; i < leg_velocities.Length; i++) // Decomposing leg velocities during instance construction
@@ -73,8 +73,8 @@ namespace BattleSystemTest_Aug9
                 float euclid_len;
                 float x_len;
                 float y_len;
-                x_len = this.FlightPath[i + 1][0] - this.FlightPath[i][0]; // x2 - x1
-                y_len = this.FlightPath[i + 1][1] - this.FlightPath[i][1]; // y2 - y1
+                x_len = this.VehiclePath[i + 1][0] - this.VehiclePath[i][0]; // x2 - x1
+                y_len = this.VehiclePath[i + 1][1] - this.VehiclePath[i][1]; // y2 - y1
                 euclid_len = (float)Math.Sqrt((x_len * x_len) + (y_len * y_len)); // Euclidean distance formule
                 vel[0] = this.LegVelocities[i] * (x_len / euclid_len); // x_vel = vel * cos(angle)
                 vel[1] = this.LegVelocities[i] * (y_len / euclid_len); // y_vel = vel * sin(angle)
@@ -99,44 +99,68 @@ namespace BattleSystemTest_Aug9
         {
             BattleSOS.SystemsOnField = new List<BattleSystem>
             { new Aircraft(new List<float[]> {new float[] { 0.0f, 0.0f },
-                                              new float[] { 4.0f, 3.0f },
-                                              new float[] { 16.0f, 3.0f },
+                                              new float[] { 5.0f, 4.0f },
+                                              new float[] { 15.0f, 4.0f },
                                               new float[] { 20.0f, 0.0f } }, new float[] { 1.0f, 2.0f, 1.5f }),
             }; // Argument 1 = List of floating point arrays of waypoints. Argument 2 = Floating point array of leg velocities
         }
+
+        public void RegisterVehicle(BattleSystem vehicle)
+        {
+            BattleSOS.SystemsOnField.Add(vehicle);
+        }
+
         public void RunSimulationEngine()
         {
             List<float[]> globalSituationAwareness = new List<float[]>();
 
 
 
-            foreach (var system in BattleSOS.SystemsOnField)
+            foreach (var vehicle in BattleSOS.SystemsOnField)
             {
-                globalSituationAwareness.Add(system.Get());
+                globalSituationAwareness.Add(vehicle.Get());
             }
 
-            foreach (var system in BattleSOS.SystemsOnField)
+            foreach (var vehicle in BattleSOS.SystemsOnField)
             {
-                system.OnTick(1.0f);
+                vehicle.OnTick(1.0f);
             }
 
-            foreach (var system in BattleSOS.SystemsOnField)
+            foreach (var vehicle in BattleSOS.SystemsOnField)
             {
-                for (int i = 0; i < system.LegVelocities.Length; i++)
+                for (int i = 0; i < vehicle.LegVelocities.Length; i++)
                 {
-                    if (system.CurrentPosition[0] >= system.FlightPath[i][0])
+                    if (vehicle.VehiclePath[i][0] > vehicle.VehiclePath[i + 1][0])
                     {
-                        system.InLeg = i;
+                        if (vehicle.CurrentPosition[0] <= vehicle.VehiclePath[i][0])
+                        {
+                            vehicle.InLeg = i;
+                        }
+                        if ((vehicle.InLeg == vehicle.Velocities.Count - 1)
+                            && vehicle.CurrentPosition[0] <= vehicle.VehiclePath[i + 1][0])
+                        {
+                            // Checks whether the Vehicle is in last leg and has stopped
+                            vehicle.VehicleHasStopped = true;
+                        }
                     }
-                    if (system.CurrentPosition[1] < 0) // Checks whether the Aircraft is on the ground
+                    else
                     {
-                        system.FlightHasStopped = true;
+                        if (vehicle.CurrentPosition[0] >= vehicle.VehiclePath[i][0])
+                        {
+                            vehicle.InLeg = i;
+                        }
+                        if ((vehicle.InLeg == vehicle.Velocities.Count - 1)
+                            && vehicle.CurrentPosition[0] >= vehicle.VehiclePath[i + 1][0])
+                        {
+                            // Checks whether the Vehicle is in last leg and has stopped
+                            vehicle.VehicleHasStopped = true;
+                        }
                     }
                 }
-                if (!system.FlightHasStopped) // If Aircraft is on the ground, stop clculating new values for position
+                if (!vehicle.VehicleHasStopped) // If Vehicle has stopped, stop calculating new values for position
                 {
-                    system.Set();
-                    Console.WriteLine($"({system.CurrentPosition[0]},{system.CurrentPosition[1]})");
+                    vehicle.Set();
+                    Console.WriteLine($"({vehicle.CurrentPosition[0]},{vehicle.CurrentPosition[1]})");
                 }
             }
         }
