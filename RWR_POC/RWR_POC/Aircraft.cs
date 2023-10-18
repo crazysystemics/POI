69dd1ae092bf0e7cb2a39df898184c808b65426c
RohitChaoji <rohitchaoji@gmail.com>
2023-10-18 15:18:15 +0530

    Added some logic to calculate new waypoints but have not yet implemeted it

--- RWR_POC/RWR_POC/Aircraft.cs
+++ RWR_POC/RWR_POC/Aircraft.cs
@@ -5,6 +5,10 @@ class Aircraft : BattleSystem
     public override bool Stopped { get; set; }
     public RWR rwr;
     public Dictionary<int, Position> waypoints = new Dictionary<int, Position>();
+    public int currentWaypointID = 0;
+    public int nextWaypointID = 1;
+    public int[] legVelocity = new int[2];
+    public Position nextWaypoint;
     //   public Position currentPosition = new Position(0, 0);
 
     public class Out : OutParameter
@@ -49,12 +53,41 @@ class Aircraft : BattleSystem
             Console.WriteLine($"Aircraft {id}: \tPosition (x, y): ({position.x}, {position.y})\n");
             Globals.aircraftDebugPrint = false;
         }
+        computeVelocity();
+        if (computeDistance(position, nextWaypoint) <= 1)
+        {
+            // Change waypoint
+            // this.currentWaypointID = this.nextWaypointID
+        }
+        else
+        {
+            // Move aircraft
+            // this.position.x += (legVelocity[0] * 1)
+            // this.position.y += (legVelocity[0] * 1)
+            // How and where to set this?
+        }
+    }
+
+    public int computeDistance(Position pos1, Position pos2)
+    {
+        int dist = (int)Math.Sqrt(((pos2.x - pos1.x) * (pos2.x - pos1.x)) + ((pos2.y - pos1.y) * (pos2.y - pos1.y)));
+        return dist;
+    }
+
+    public int[] computeVelocity()
+    {
+        int x_dist = Math.Abs(this.waypoints[currentWaypointID].x - this.waypoints[nextWaypointID].x);
+        int y_dist = Math.Abs(this.waypoints[currentWaypointID].y - this.waypoints[nextWaypointID].y);
+        this.legVelocity[0] = x_dist / (nextWaypointID - currentWaypointID);
+        this.legVelocity[1] = y_dist / (nextWaypointID - currentWaypointID);
+        return legVelocity;
     }
 
     public Aircraft(Dictionary<int, Position> waypoints, int id)
     {
         this.position = waypoints[0];
         this.waypoints = waypoints;
+        this.nextWaypoint = waypoints[1];
         this.id = id;
     }
 }
