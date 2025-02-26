a90df5c3ed172d18fc8b8ba786db226d60e55683
rvjoshi18 <rvjoshi18@hotmail.com>
2025-02-26 12:27:33 +0530

    previous day work commit

--- platform_cpp/constraint_puzzle_solver/entity.cs
+++ platform_cpp/constraint_puzzle_solver/entity.cs
@@ -12,12 +12,20 @@ namespace constraint_puzzle_solver
     class entity
     {
         private string name;
-        private List<string> value;
+        private List<string> values;
+        public List<bool> isAssigned;
 
-        public entity(string name, List<string> value)
+        public entity(string name, List<string> values)
         {
             this.name = name;
-            this.value = value;
+            this.values = values;
+
+            //initialize isAssigned list to all false
+            isAssigned = new List<bool>();
+            for (int i = 0; i < values.Count; i++)
+            {
+                isAssigned.Add(false);
+            }
         }
 
         public string getname()
@@ -27,7 +35,7 @@ namespace constraint_puzzle_solver
 
         public List<string> getvalue()
         {
-            return value;
+            return values;
         }
     }
 
@@ -36,20 +44,46 @@ namespace constraint_puzzle_solver
     class entitytree
     {
         List<entity> entities = new List<entity>();
+        int entity_index = 0;
+        int value_index = 0;
         public entitytree() { }
 
-        //traverse entities in depth first order
-        public void traverse()
+        //modify traverse method to traverse one step at a time
+        //and return the entity
+        public void getLatestAssignment(ref entity e, ref string eval)
         {
-            foreach (entity e in entities)
+            e = entities[entity_index];  
+            eval = entities[entity_index].getvalue()[value_index]; 
+        } 
+        
+        public void Assign()
+        {
+            entities[entity_index].isAssigned[value_index] = true;
+        }
+           
+        public void traverse_one_step()
+        {
+            entities[entity_index].isAssigned[value_index] = false;
+            
+            entity e = entities[entity_index];            
+            if (value_index < e.getvalue().Count)
             {
-                //Console.WriteLine(e.getname());
-                foreach (string s in e.getvalue())
+                value_index++;
+            }
+            else
+            {
+                entity_index++;
+                if (entity_index == entities.Count)
                 {
-                    Console.WriteLine(e.getname() + ":" + s);
+                    entity_index = -1;
                 }
+                value_index = 0;
             }
         }
+
+       
+
+
 
         public void addentity(entity entity)
         {
